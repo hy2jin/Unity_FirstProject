05e06f79328016b9baf5e2c3abf50bc8fa10cf20
hyejin <hy2jinl22@gmail.com>
2024-01-26 10:00:35 +0900

    일정 범위에 도달하면 적과 총알 삭제

---
+++ Assets/Scripts/ShootingGame/DestroyZone.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // ºÎµúÈù ´ë»ó Á¦°Å
+        Destroy(other.gameObject);
+    }
+}
