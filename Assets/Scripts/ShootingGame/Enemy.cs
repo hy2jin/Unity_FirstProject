d559e408825848082a263f6aeeb0b653ab69b818
hyejin <hy2jinl22@gmail.com>
2024-01-26 10:46:05 +0900

    적 폭발 효과 추가

--- Assets/Scripts/ShootingGame/Enemy.cs
+++ Assets/Scripts/ShootingGame/Enemy.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 5f;
+    // 폭발 공장
+    public GameObject explosionFactory;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,10 @@ public class Enemy : MonoBehaviour
     // 충돌 감지
     private void OnCollisionEnter(Collision collision)
     {
+        // 폭발 효과 생성
+        GameObject explosion = Instantiate(explosionFactory);
+        explosion.transform.position = transform.position;
+
         // 적 오브젝트 제거 (내 게임 오브젝트)
         Destroy(gameObject);
         // 플레이어 오브젝트 제거 (충돌 대상)
